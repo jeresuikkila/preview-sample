7e9a9e488ecb3b3159acf0fb3f2bc5ed08b2d7e1
Jere Suikkila <jere@suikkila.com>
2023-08-06 14:19:15 -0400

    add handlebars

--- scripts/build-examples.ts
+++ scripts/build-examples.ts
@@ -1,16 +1,22 @@
 import * as esbuild from 'esbuild';
 import fs from 'node:fs'
+import Handlebars from 'handlebars';
+
+console.log(process.env.HASH);
 
 async function build() {
 
-await esbuild.build({
-    entryPoints: ['examples/index.js'],
-    bundle: true,
-    outdir: 'examples/dist'
-})
+    await esbuild.build({
+        entryPoints: ['examples/index.js'],
+        bundle: true,
+        outdir: 'examples/dist'
+    })
 
-fs.copyFileSync('examples/index.html', 'examples/dist/index.html')
-
+    const templateFile = fs.readFileSync('examples/index.html', {encoding: 'utf-8'})
+    const template = Handlebars.compile(templateFile)
+    fs.writeFileSync('examples/dist/index.html', template({
+        gitCommit: process.env.HASH || 'git hash unknown'
+    }))
 
 }
 
